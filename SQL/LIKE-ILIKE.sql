260580c3ea9f04b31db84aae0c90ed8636dcf7d7
Ali Ihsan Ozkaya <ihsanozkaya1903@gmail.com>
2024-07-01 18:03:57 +0300

    odev3 yapimi

---
+++ SQL/LIKE-ILIKE.sql
@@ -0,0 +1,20 @@
+-- SELECT * FROM customer WHERE first_name LIKE 'Ma%';
+
+-- SELECT * FROM customer WHERE first_name LIKE '%y';
+
+-- SELECT * FROM customer WHERE first_name LIKE 'A%y';
+
+-- SELECT * FROM customer WHERE first_name LIKE 'A%' AND last_name LIKE 'A%';
+
+-- SELECT * FROM customer WHERE first_name ILIKE 'a%';
+
+-- SELECT * FROM customer WHERE first_name ILIKE '%K';
+
+-- SELECT * FROM customer WHERE first_name NOT LIKE 'D%n';
+
+-- SELECT * FROM customer WHERE first_name NOT LIKE 'A%';
+
+-- SELECT * FROM customer WHERE first_name LIKE 'J_an';
+
+-- SELECT * FROM customer WHERE first_name LIKE 'J_';
+
