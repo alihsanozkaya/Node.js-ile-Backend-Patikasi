7aeed1c06fab9cfe6491006cdb7e5a8b1d4d4b18
Ali Ihsan Ozkaya <ihsanozkaya1903@gmail.com>
2024-07-01 18:39:33 +0300

    odev 4 yapildi

---
+++ SQL/DISTINCT-COUNT.sql
@@ -0,0 +1,13 @@
+-- SELECT DISTINCT rental_rate FROM film;
+
+-- SELECT DISTINCT replacement_cost FROM film;
+
+-- SELECT DISTINCT country FROM country;
+
+-- SELECT COUNT(*) FROM actor;
+
+-- SELECT COUNT(*) FROM actor WHERE first_name LIKE 'A%';
+
+-- SELECT COUNT(DISTINCT first_name) FROM actor;
+
+-- SELECT COUNT(DISTINCT length) FROM film;
